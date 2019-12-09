8a72b4c871c6079ca2244ef40c1b6c3c96bb427f
kishaihd <mr.pinstripe_suit@hotmail.com>
2019-12-09 16:33:05 -0700

    Porting starwars stuff to overwatch

--- Overwatch/overwatch.js
+++ Overwatch/overwatch.js
@@ -1,3 +1,37 @@
+(async function() {
+    let jsonData = await getAPIData('https://overwatch-api.net/api/v1/hero/');
+    let characterData = [];
+    for (const stats in jsonData['data']) {
+        var c = new OWCharacter(stats['name'], stats['description']);
+        characterData.push(c);
+    }
+    await Promise.all(characterData);
+    populateDOM(newCard)
+});
+
+class OWCharacter {
+    constructor(nameVar, descriptionVar) {
+      this.name = name; 
+      this.description = description;
+      this.picSource = "https://d1u1mce87gyfbn.cloudfront.net/hero/${name}/hero-select-portrait.png"
+    }
+}
+  
+  const newButton = document.querySelector('#newCard');
+  newButton.addEventListener("click", function() {
+      let charId = prompt("Please enter a character ID");
+      console.log(typeof charId);
+      if (charId > 0 && charId <= 20) { // Or however many there are. 
+        getAPIData`'https://overwatch-api.net/api/v1/hero/${charId}`)
+      .then(result => {
+      populateDOM(result);
+      });
+    } else {
+        alert('There are no characters with that number. Try another one.');
+    }
+  }) 
+
+
 async function getAPIData (url) {
     try {
         const response = await fetch(url)
@@ -6,17 +40,106 @@ async function getAPIData (url) {
     } catch (error) {
         console.log(error)
     }
-    }
+}
     
     // now,use the returned async data
-    const theData = getAPIData('https://overwatch-api.net/api/v1')
+    const theData = getAPIData('https://overwatch-api.net/api/v1/hero/')
     .then(data => {
-        for (const character of data.results) {
-            getAPIData(character.url)
-            .then(characterdata => {
-                populateDOM(characterdata)
-            })
-    }
+        for (const c of data['data']) {
+            var c = new OWCharacter(stat['name'], stat['description']);
+            characterList.push(c);
+        }
     })
-
+    
+    
     let mainArea = document.querySelector('main')
+    let nav = document.querySelector('nav')
+    // populateDOM(characterList)
+    function populateDOM(single_hero) {
+        let heroScene = document.createElement('div')
+        let heroCard = document.createElement('div')
+        let heroFront = document.createElement('div')
+        let heroBack = document.createElement('div')
+        let pic = document.createElement('img')
+        let picContainer = document.createElement('div')
+        let name = document.createElement ('h1')
+        name.textContent = single_hero.name
+        let type = document.createElement('p')
+        let heroDiv = document.createElement('div')
+    
+        fillCardBack(heroBack, single_hero)
+        //
+        heroScene.setAttribute('class', 'scene')
+        heroCard.setAttribute('class', 'card')
+        heroFront.setAttribute('class', 'card_face card_face--front')
+        heroBack.setAttribute('class', 'card_face card_face--back')
+        pic.setAttribute('class', 'picDivs')
+        picContainer.setAttribute('class', 'containerDivs')
+    
+        name.textContent = `${single_hero.name}`
+     
+       // pic.src = `../assets/images/${heroNum}.png`
+       pic.src = `src="https://d1u1mce87gyfbn.cloudfront.net/hero/${single_hero.name}/hero-select-portrait.png`;
+
+     
+        heroFront.appendChild(picContainer)
+        heroFront.appendChild(name)
+        picContainer.appendChild(pic)
+    
+        heroCard.appendChild(heroFront)
+        heroCard.appendChild(heroBack)
+        heroScene.appendChild(heroCard)
+    
+        mainArea.appendChild(heroScene)
+        console.log(name)
+    
+        heroScene.setAttribute('class', 'scene')
+        heroCard.setAttribute('class', 'card')
+        heroFront.setAttribute('class', 'card__face card__face--front')
+        heroBack.setAttribute('class', 'card__face card__face--back')
+        name.setAttribute('class', 'charName')
+        pic.setAttribute('class', 'charPic')
+        //var heroCard = document.querySelector('.card');
+    
+    
+        heroCard.addEventListener( 'click', function() {
+          heroCard.classList.toggle('is-flipped');
+        });
+    
+    }
+    
+    
+    
+    
+    //function getPokeNumber(id) {
+    //    if (id < 10) return `00${id}`
+    //    if(id > 9 && id < 100) {
+    //        return `0${id}`
+    //    } else return id
+    //}
+    
+    function fillCardBack(heroBack, data) {
+        let heroDescription = document.createElement('p')
+        //let heroOrder = document.createElement('p')
+        //let heroWeight = document.createElement('p')
+        //let heroHeight = document.createElement ('p')
+        heroDescription.textContent = `${data.description}`
+        //heroOrder.textContent = `Number: ${data.order}`
+        //heroWeight.textContent = `Weight: ${data.weight}`
+        //heroHeight.textContent = `Height: ${data.height}`
+        heroBack.appendChild(heroDescription)
+        //heroBack.appendChild(heroOrder)
+        //heroBack.appendChild(heroWeight)
+        //heroBack.appendChild(heroHeight) 
+    }
+    
+    
+    
+      /*  let end = charURL.lastIndexOf('/')
+        let charID = charURL.substring (end -2, end)
+        if(charID.indexOf('/') !== -1) {
+           return `00${charID.slice(1, 2)}`
+        }   else {
+            return `0${charID}`
+        }  
+    } */
